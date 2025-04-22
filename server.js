4b34d5d049a1528485d15941a542bfb4811ea005
Mahmoud Gewily <mahmoud.a.gewily@gmail.com>
2025-04-22 19:44:06 +0300

    configs

--- server.js
+++ server.js
@@ -1,22 +1,9 @@
-// JSON Server module
 const jsonServer = require("json-server");
 const server = jsonServer.create();
-const router = jsonServer.router("db/db.json");
-
-// Make sure to use the default middleware
+const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
-// Add this before server.use(router)
-server.use(
-  // Add custom route here if needed
-  jsonServer.rewriter({
-    "/api/*": "/$1",
-  }),
-);
 server.use(router);
-// Listen to port
-server.listen(3000);
 
-// Export the Server API
 module.exports = server;
